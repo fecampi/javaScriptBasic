059c9cfc20a17d1d54c3819ee345c96177732712
Felipe Campinho <felipe.campinho@mirakulo.com>
2022-05-21 18:39:32 -0300

    requisições

--- Objects/ObjectsIntro.js
+++ Objects/ObjectsIntro.js
@@ -28,6 +28,32 @@ let person3 = Object.assign({}, person2)
 console.log(person1["name"]);
 console.log(person3.age);
 
+//---Object Map---
+const persons = [
+  { id: 1, name: 'Felipe' },
+  { id: 2, name: 'Carlos' },
+  { id: 3, name: 'Luiz' }
+]
+//use atribute direct
+const newPersons = {}
+for (const person of persons) {
+  const { id } = person
+  newPersons[id] = { ...person}
+}
+//use set
+const newPersonsMap = new Map
+for (const person of persons) {
+  const { id } = person
+  newPersonsMap.set(id, {...person})
+}
+//use delete
+newPersonsMap.delete(3)
+
+
+
+console.log(newPersons)
+console.log(newPersonsMap)
+
 
 /* Transformar objeto em Global(exportação ideal para reutilizar em outro arquivo)
     (function (win, doc) {
--- Requisições/Axios(simples)/axios.js
+++
@@ -1,8 +0,0 @@
-axios.get('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json', {})
-        .then(response => {
-            console.log(response)
-            console.log(response.data.members)
-        })
-        .catch(error => {
-            console.log(error)
-        })
